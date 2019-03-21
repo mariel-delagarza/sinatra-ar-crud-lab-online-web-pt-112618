ab9569e0bacc31c6cdaed321cf5fa6c539c6cb65
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-21 14:57:00 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,4 +47,7 @@ class ApplicationController < Sinatra::Base
   delete '/articles/:id' do 
     @article=Article.find(params[:id])
     @article.destroy 
+    redirect to "/articles"
+  end 
+  
 end
