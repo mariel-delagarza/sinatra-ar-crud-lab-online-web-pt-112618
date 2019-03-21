7dc03383266288964d6f3a8ea35eef24849d51cc
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-03-21 14:47:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,11 +28,15 @@ class ApplicationController < Sinatra::Base
   end 
     
   get 'articles/:id' do 
-    @article = Article.find(params[id])
+    @article = Article.find(params[:id])
     erb :show 
   end 
   
-  
+    get "/articles/:id/edit" do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
   
   patch '/articles/:id/edit' do 
     @article = Article.find(params[:id])
